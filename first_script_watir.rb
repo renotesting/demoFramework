9a7de64c6e00ef0a338fa20abd524b37787dc42a
reno l <reno_testing@hotmail.com>
2022-01-12 11:54:43 -0700

    initial none framework script

---
+++ first_script_watir.rb
@@ -0,0 +1,24 @@
+require 'watir-webdriver'
+require 'selenium-webdriver'
+
+#caps = Selenium::WebDriver::Remote::Capabilities.chrome("goog:chromeOptions" => {detach: true})
+browser =  Watir::Browser.new #:chrome, desired_capabilities: caps
+#browser.goto 'watir.com'
+browser.goto 'www.shaw.ca'
+browser.driver.manage.window.maximize
+
+sleep 1
+browser.button(text: 'Internet', class: 'c-desktop-nav__category is-drop-down').click
+sleep 1
+browser.link(href: '/internet/plans', class: 'c-desktop-nav__sub-menu-links').click
+sleep 1
+group = browser.div(class: 'c-plans-overview__container')
+if(group.exists?)
+	browser.execute_script('arguments[0].scrollIntoView();', group)
+end
+
+puts browser.title
+
+sleep 5
+# => 'Guides – Watir Project'
+#browser.close
